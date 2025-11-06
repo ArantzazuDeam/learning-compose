06e3f73aea55ff27737501748283cd810e2f2fea
arantzazu.dealba <arantzazu.dealba@babelgroup.com>
2025-11-06 14:47:10 +0100

    CompositionLocalProvider

--- app/src/main/java/com/example/mynewcompose/ui/theme/Theme.kt
+++ app/src/main/java/com/example/mynewcompose/ui/theme/Theme.kt
@@ -9,6 +9,7 @@ import androidx.compose.material3.dynamicDarkColorScheme
 import androidx.compose.material3.dynamicLightColorScheme
 import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.CompositionLocalProvider
 import androidx.compose.runtime.ReadOnlyComposable
 import androidx.compose.runtime.SideEffect
 import androidx.compose.ui.graphics.toArgb
@@ -98,9 +99,13 @@ fun MyNewComposeAppTheme(
             else -> LightColorScheme
         }
 
-    MaterialTheme(
-        colorScheme = colorScheme,
-        typography = Typography,
-        content = content,
-    )
+    CompositionLocalProvider(
+        LocalDimens provides dimens,
+    ) {
+        MaterialTheme(
+            colorScheme = colorScheme,
+            typography = Typography,
+            content = content,
+        )
+    }
 }
